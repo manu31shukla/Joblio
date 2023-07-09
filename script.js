1aa7664b8045a80b481563aa1919364b6fdd48a7
manu31shukla <manu31shukla@gmail.com>
2023-07-09 10:36:40 +0530

    search page

--- script.js
+++ script.js
@@ -1,22 +1,127 @@
-function fetchLocation() {
-    // Implement the logic to fetch the user's location using Google Maps API or other geolocation methods
-    // Update the input value with the fetched location
-  }
+
+
+document.getElementById('search-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent form submission
   
-  function toggleJobRoleOptions() {
-    const options = document.querySelector('.job-role-options');
-    options.style.display = options.style.display === 'none' ? 'block' : 'none';
-  }
+  var location = document.getElementById('location').value;
+  var jobRole = document.getElementById('job-role').value;
   
-  function selectJobRole(option) {
-    const jobRoleInput = document.getElementById('job-role');
-    if (option === 'other') {
-      // Handle "Other" option
-      jobRoleInput.classList.add('show-other-input');
-    } else {
-      jobRoleInput.value = option.innerText;
-      jobRoleInput.classList.remove('show-other-input');
-    }
-    toggleJobRoleOptions();
-  }
+  // Simulate search functionality
+  searchCandidates(location, jobRole);
+});
+
+function searchCandidates(location, jobRole) {
+  // Fetch the list of candidates based on the search criteria (location and job role)
   
+  // Example: Dummy data for demonstration
+  var dummyCandidates = [
+    {
+      name: 'John Doe',
+      location: 'New York, USA',
+      jobRole: 'Frontend Developer'
+    },
+    {
+      name: 'Jane Smith',
+      location: 'London, UK',
+      jobRole: 'Backend Developer'
+    },
+    {
+      name: 'Alex Johnson',
+      location: 'Sydney, Australia',
+      jobRole: 'Full Stack Developer'
+    },
+    {
+      name: "Sarah Thompson",
+      location: "Toronto, Canada",
+      jobRole: "Software Engineer"
+    },
+    {
+      name: "Michael Brown",
+      location: "Berlin, Germany",
+      jobRole: "Data Scientist"
+    },
+    {
+      name: "Emily Wilson",
+      location: "Paris, France",
+      jobRole: "Graphic Designer"
+    },
+    {
+      name: "Daniel Lee",
+      location: "Tokyo, Japan",
+      jobRole: "UX/UI Designer"
+    },
+    {
+      name: "Sara aLI",
+      location: "New Delhi, India",
+      jobRole: "Mechanical Engineer"
+    },
+    {
+      name: "Manu Shukla",
+      location: "Hyderabad, India",
+      jobRole: "Devops Engineer"
+    },
+    {
+      name: "Mohit Singh",
+      location: "Chennai, India",
+      jobRole: "AI/ML Engineer"
+    },
+    {
+      name: "Daniel Patel",
+      location: "Mumbai, India",
+      jobRole: "Graphic Designer"
+    },
+    // Add more dummy candidates here
+  ];
+  
+  // Filter the candidates based on the search criteria
+  var filteredCandidates = dummyCandidates.filter(function(candidate) {
+    return (location === '' || candidate.location.toLowerCase().includes(location.toLowerCase())) &&
+           (jobRole === '' || candidate.jobRole.toLowerCase().includes(jobRole.toLowerCase()));
+  });
+  
+  // Display the filtered candidates
+  displayCandidates(filteredCandidates);
+}
+
+function displayCandidates(candidates) {
+  var searchResults = document.getElementById('search-results');
+  searchResults.innerHTML = ''; // Clear the previous search results
+  
+  var resultsHeading = document.createElement('h2');
+  resultsHeading.textContent = 'Search Results';
+  searchResults.appendChild(resultsHeading);
+  
+  if (candidates.length === 0) {
+    var noResultsMessage = document.createElement('p');
+    noResultsMessage.textContent = 'No candidates found.';
+    searchResults.appendChild(noResultsMessage);
+  } else {
+    var candidatesList = document.createElement('ul');
+    
+    candidates.forEach(function(candidate) {
+      var candidateCard = document.createElement('li');
+      candidateCard.className = 'candidate-card';
+      
+      var candidateName = document.createElement('h3');
+      candidateName.textContent = candidate.name;
+      candidateCard.appendChild(candidateName);
+      
+      var location = document.createElement('p');
+      location.textContent = 'Location: ' + candidate.location;
+      candidateCard.appendChild(location);
+      
+      var jobRole = document.createElement('p');
+      jobRole.textContent = 'Job Role: ' + candidate.jobRole;
+      candidateCard.appendChild(jobRole);
+      
+      var evaluateButton = document.createElement('button');
+      evaluateButton.className = 'evaluate-button';
+      evaluateButton.textContent = 'Evaluate';
+      candidateCard.appendChild(evaluateButton);
+      
+      candidatesList.appendChild(candidateCard);
+    });
+    
+    searchResults.appendChild(candidatesList);
+  }
+}
